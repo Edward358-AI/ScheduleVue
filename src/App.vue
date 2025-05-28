70642580661e0f88e72ddeffa097b22f3d562328
Edward Jiang <66029887+Edward358-AI@users.noreply.github.com>
2025-05-28 06:36:35 +0000

    github is being weird

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ const user = reactive({
   theme: "light"
 })
 
-if(localStorage.getItem("user")) {
+if (localStorage.getItem("user")) {
   user.theme = JSON.parse(localStorage.getItem("user")).theme;
 }
 else {
@@ -167,15 +167,15 @@ const senior = computed(() => {
 
 function bytesArrToBase64(arr) {
   const abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"; // base64 alphabet
-  const bin = n => n.toString(2).padStart(8,0); // convert num to 8-bit binary string
+  const bin = n => n.toString(2).padStart(8, 0); // convert num to 8-bit binary string
   const l = arr.length
   let result = '';
 
-  for(let i=0; i<=(l-1)/3; i++) {
-    let c1 = i*3+1>=l; // case when "=" is on end
-    let c2 = i*3+2>=l; // case when "=" is on end
-    let chunk = bin(arr[3*i]) + bin(c1? 0:arr[3*i+1]) + bin(c2? 0:arr[3*i+2]);
-    let r = chunk.match(/.{1,6}/g).map((x,j)=> j==3&&c2 ? '=' :(j==2&&c1 ? '=':abc[+('0b'+x)]));  
+  for (let i = 0; i <= (l - 1) / 3; i++) {
+    let c1 = i * 3 + 1 >= l; // case when "=" is on end
+    let c2 = i * 3 + 2 >= l; // case when "=" is on end
+    let chunk = bin(arr[3 * i]) + bin(c1 ? 0 : arr[3 * i + 1]) + bin(c2 ? 0 : arr[3 * i + 2]);
+    let r = chunk.match(/.{1,6}/g).map((x, j) => j == 3 && c2 ? '=' : (j == 2 && c1 ? '=' : abc[+('0b' + x)]));
     result += r.join('');
   }
 
@@ -186,11 +186,11 @@ function base64ToBytesArr(str) {
   const abc = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"]; // base64 alphabet
   let result = [];
 
-  for(let i=0; i<str.length/4; i++) {
-    let chunk = [...str.slice(4*i,4*i+4)]
-    let bin = chunk.map(x=> abc.indexOf(x).toString(2).padStart(6,0)).join(''); 
-    let bytes = bin.match(/.{1,8}/g).map(x=> +('0b'+x));
-    result.push(...bytes.slice(0,3 - (str[4*i+2]=="=") - (str[4*i+3]=="=")));
+  for (let i = 0; i < str.length / 4; i++) {
+    let chunk = [...str.slice(4 * i, 4 * i + 4)]
+    let bin = chunk.map(x => abc.indexOf(x).toString(2).padStart(6, 0)).join('');
+    let bytes = bin.match(/.{1,8}/g).map(x => +('0b' + x));
+    result.push(...bytes.slice(0, 3 - (str[4 * i + 2] == "=") - (str[4 * i + 3] == "=")));
   }
   return result;
 }
@@ -325,7 +325,8 @@ watch(user, () => {
     <span>Import/Export Course Data: </span><br>
     <button class="btn btn-secondary me-3" @click="exportData()">Export</button>
     <button class="btn btn-secondary me-3" @click="importData()">Import</button>
-    <button class="btn btn-secondary" @click='WINDOW.localStorage.clear();WINDOW.location.reload()'>Clear Data</button><br><br>
+    <button class="btn btn-secondary" @click='WINDOW.localStorage.clear(); WINDOW.location.reload()'>Clear
+      Data</button><br><br>
     <textarea v-model="data">{{ data }}</textarea><br>
     <span>{{ dataMessage }}</span>
   </div>
