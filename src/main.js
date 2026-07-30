5bb655b0c234e48374cbffd5a97e9d247722b17e
Edward Jiang <edward456jiang@gmail.com>
2026-07-30 13:56:05 -0700

    fix build issue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import classes from "@/classes.js"
+import classes from "./classes.js"
 
 createApp(App).mount('#app')
